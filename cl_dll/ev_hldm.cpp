31e2e96469e1a1fb981a171d0ed64c84fa16e3e9
Glitch31415 <123266631+Glitch31415@users.noreply.github.com>
2024-10-14 16:37:43 -0700

    untest

--- cl_dll/ev_hldm.cpp
+++ cl_dll/ev_hldm.cpp
@@ -547,7 +547,6 @@ void EV_FireGlock2( event_args_t *args )
 //======================
 void EV_FireShotGunDouble( event_args_t *args )
 {
-	gEngfuncs.Con_DPrintf("FireShotGunDouble");
 
 	int idx;
 	vec3_t origin;
