a732deb6c75b52efb59213d2da82a3a8961d0607
Glitch31415 <123266631+Glitch31415@users.noreply.github.com>
2024-10-26 00:37:09 -0700

    fix???????

--- dlls/monster/CBaseGrunt.cpp
+++ dlls/monster/CBaseGrunt.cpp
@@ -1607,7 +1607,7 @@ Schedule_t	slGruntSweep[] =
 // primary range attack. Overriden because base class stops attacking when the enemy is occluded.
 // grunt's grenade toss requires the enemy be occluded.
 //=========================================================
-static float reactiontim = RANDOM_FLOAT(0.0, 0.5);
+static float reactiontim = 0.5;
 Task_t	tlGruntRangeAttack1A[] =
 {
 
