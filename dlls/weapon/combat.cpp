24048dd01212ad8d58a02292a0098d2b0c079de8
Glitch31415 <123266631+Glitch31415@users.noreply.github.com>
2024-10-28 18:06:09 -0700

    fix

--- dlls/weapon/combat.cpp
+++ dlls/weapon/combat.cpp
@@ -92,6 +92,9 @@ void RadiusDamage( Vector vecSrc, entvars_t *pevInflictor, entvars_t *pevAttacke
 				else {
 					distance = (((((-1 * distance) / 10) / (drf - 200)) * distance) - ((pow(distance, 2))/1000)) + distance;
 				}
+				if (distance < 0) {
+					distance = 0;t
+				}
 				flAdjustedDamage = (flDamage/(distance * 0.00318198051534)) - (distance * 0.00795495128835);
 				//flAdjustedDamage = flDamage - flAdjustedDamage;
 			
