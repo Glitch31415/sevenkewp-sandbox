90658f52f526e222ac2f833fca4a809883f901ff
Glitch31415 <123266631+Glitch31415@users.noreply.github.com>
2024-11-03 01:59:22 -0700

    test

--- dlls/CBasePlayer.cpp
+++ dlls/CBasePlayer.cpp
@@ -1311,6 +1311,7 @@ void CBasePlayer::WaterMove()
 	int air;
 	int bci;
 	bci = roundf(pev->air_finished - gpGlobals->time);
+	UTIL_ClientPrintAll(print_chat, UTIL_VarArgs("bci: %i\n", bci));
 	if (pev->movetype == MOVETYPE_NOCLIP)
 		return;
 
@@ -1327,19 +1328,19 @@ void CBasePlayer::WaterMove()
 		// not underwater
 		
 		// play 'up for air' sound
-		if (pev->air_finished < gpGlobals->time)
-			EMIT_SOUND(ENT(pev), CHAN_VOICE, "player/pl_wade1.wav", 1, ATTN_NORM);
-		else if (pev->air_finished < gpGlobals->time + 15)
-			EMIT_SOUND(ENT(pev), CHAN_VOICE, "player/pl_wade2.wav", 1, ATTN_NORM);
+		if (pev->waterlevel == 2) {
+			if (pev->air_finished < gpGlobals->time)
+				EMIT_SOUND(ENT(pev), CHAN_VOICE, "player/pl_wade1.wav", 1, ATTN_NORM);
+			else if (pev->air_finished < gpGlobals->time + 15)
+				EMIT_SOUND(ENT(pev), CHAN_VOICE, "player/pl_wade2.wav", 1, ATTN_NORM);
+		}
+
 		if (pev->pain_finished < gpGlobals->time) {
-			UTIL_ClientPrintAll(print_chat, UTIL_VarArgs("not underwater, before change bci: %i\n", bci));
 			bci = bci + 2;
-			UTIL_ClientPrintAll(print_chat, UTIL_VarArgs("not underwater, after change bci: %i\n", bci));
 			if (bci > AIRTIME) {
 				bci = AIRTIME;
 			}
 			pev->air_finished = gpGlobals->time + bci;
-			UTIL_ClientPrintAll(print_chat, UTIL_VarArgs("not underwater, air_finished diff: %f\n", pev->air_finished-gpGlobals->time));
 			pev->pain_finished = gpGlobals->time + 1;
 			
 		}
@@ -1370,15 +1371,12 @@ void CBasePlayer::WaterMove()
 
 		if (pev->pain_finished < gpGlobals->time)
 		{
-			UTIL_ClientPrintAll(print_chat, UTIL_VarArgs("underwater, before change bci: %i\n", bci));
-			bci = bci - 1;
-			UTIL_ClientPrintAll(print_chat, UTIL_VarArgs("underwater, before change bci: %i\n", bci));
-			if (bci <= 0) {
-				bci = 0;
+			//bci = bci - 1;
+			//if (bci <= 0) {
+				//bci = 0;
 				// start fucking drowning already
-			}
-			pev->air_finished = gpGlobals->time + bci;
-			UTIL_ClientPrintAll(print_chat, UTIL_VarArgs("underwater, air_finished diff: %f\n", pev->air_finished-gpGlobals->time));
+			//}
+			//pev->air_finished = gpGlobals->time + bci;
 			if (pev->air_finished <= gpGlobals->time)		// drown!
 			{
 				bci = 0;
