6ebe90d6bbbbdcde918e268f7a73e773e1690368
Glitch31415 <123266631+Glitch31415@users.noreply.github.com>
2024-12-04 16:01:45 -0800

    test

--- dlls/weapon/CPython.cpp
+++ dlls/weapon/CPython.cpp
@@ -196,14 +196,21 @@ void CPython::PrimaryAttack()
 
 	UTIL_MakeVectors( m_pPlayer->pev->v_angle + m_pPlayer->pev->punchangle );
 
-	Vector vecSrc	 = m_pPlayer->GetGunPosition( );
-	Vector vecAiming = m_pPlayer->GetAutoaimVector( AUTOAIM_10DEGREES );
+	vecAiming = gpGlobals->v_forward;
+	Vector vecSrc = m_pPlayer->GetGunPosition( ); // + gpGlobals->v_up * -8 + gpGlobals->v_right * 8;
+	Vector vecDir = vecAiming + m_pPlayer->FireBulletsPlayer( 1, vecSrc, vecAiming, VECTOR_CONE_357, 131072, BULLET_PLAYER_357, 0, 0, m_pPlayer->pev, m_pPlayer->random_seed );
+		Vector vecDest = vecSrc + vecDir * 8192;
+	edict_t		*pentIgnore;
+	TraceResult tr, beam_tr;
+	float flMaxFrac = 1.0;
+	float dmg_mult = GetDamageModifier();
 
-	lagcomp_begin(m_pPlayer);
-	Vector vecDir;
-	vecDir = m_pPlayer->FireBulletsPlayer( 1, vecSrc, vecAiming, VECTOR_CONE_357, 131072, BULLET_PLAYER_357, 0, 0, m_pPlayer->pev, m_pPlayer->random_seed );
-	lagcomp_end();
+	float flDamage = gSkillData.sk_plr_357_bullet * dmg_mult;
+	int fHasPunched = 0;
+	int fFirstBeam = 1;
+	int	nMaxHits = 5;
 
+	pentIgnore = m_pPlayer->edict();
 	PLAY_DISTANT_SOUND(m_pPlayer->edict(), DISTANT_357);
 
     int flags;
@@ -214,6 +221,134 @@ void CPython::PrimaryAttack()
 #endif
 
 	PLAYBACK_EVENT_FULL( flags, m_pPlayer->edict(), m_usFirePython, 0.0, (float *)&g_vecZero, (float *)&g_vecZero, vecDir.x, vecDir.y, 0, 0, 0, 0 );
+
+	lagcomp_begin(m_pPlayer);
+	
+	while (flDamage > 1 && nMaxHits > 0)
+	{
+
+
+		// ALERT( at_console, "." );
+		UTIL_TraceLine(vecSrc, vecDest, dont_ignore_monsters, pentIgnore, &tr);
+
+		//if (tr.fAllSolid)
+			//break;
+
+		CBaseEntity *pEntity = CBaseEntity::Instance(tr.pHit);
+
+		if (pEntity == NULL)
+			break;
+
+		if ( fFirstBeam )
+		{
+			m_pPlayer->pev->effects |= EF_MUZZLEFLASH;
+			fFirstBeam = 0;
+	
+
+		}
+		
+		if (pEntity->pev->takedamage)
+		{
+			ClearMultiDamage();
+
+			// if you hurt yourself clear the headshot bit
+
+			pEntity->TraceAttack( m_pPlayer->pev, flDamage, vecDir, &tr, DMG_BULLET );
+			ApplyMultiDamage(m_pPlayer->pev, m_pPlayer->pev);
+		}
+
+		if ( pEntity->ReflectGauss() )
+		{
+			//pentIgnore = NULL;
+
+			float n = -DotProduct(tr.vecPlaneNormal, vecDir);
+
+			if (n < 0.5) // 60 degrees
+			{
+				// ALERT( at_console, "reflect %f\n", n );
+				// reflect
+				Vector r;
+			
+				r = 2.0 * tr.vecPlaneNormal * n + vecDir;
+				flMaxFrac = flMaxFrac - tr.flFraction;
+				vecDir = r;
+				vecSrc = tr.vecEndPos + vecDir * 8;
+				vecDest = vecSrc + vecDir * 8192;
+
+				// explode a bit
+				//m_pPlayer->RadiusDamage( tr.vecEndPos, pev, m_pPlayer->pev, flDamage * n, CLASS_NONE, DMG_BLAST );
+
+
+				
+				// lose energy
+				if (n == 0) n = 0.1;
+				flDamage = flDamage * (1 - n);
+			}
+			else
+			{
+
+
+				// limit it to one hole punch
+				if (fHasPunched == 3)
+					break;
+				fHasPunched = fHasPunched + 1;
+
+				// try punching through wall if secondary attack (primary is incapable of breaking through)
+				//if ( !m_fPrimaryFire )
+				//{
+					UTIL_TraceLine( tr.vecEndPos + vecDir * 8, vecDest, dont_ignore_monsters, pentIgnore, &beam_tr);
+					if (!beam_tr.fAllSolid)
+					{
+						// trace backwards to find exit point
+						UTIL_TraceLine( beam_tr.vecEndPos, tr.vecEndPos, dont_ignore_monsters, pentIgnore, &beam_tr);
+
+						n = (beam_tr.vecEndPos - tr.vecEndPos).Length( );
+
+						if (n < flDamage)
+						{
+							if (n == 0) n = 1;
+							flDamage -= n;
+
+							// ALERT( at_console, "punch %f\n", n );
+
+
+							// exit blast damage
+							//m_pPlayer->RadiusDamage( beam_tr.vecEndPos + vecDir * 8, pev, m_pPlayer->pev, flDamage, CLASS_NONE, DMG_BLAST );
+
+							//::RadiusDamage( beam_tr.vecEndPos + vecDir * 8, pev, m_pPlayer->pev, flDamage, damage_radius, CLASS_NONE, DMG_BLAST );
+
+
+
+
+							vecSrc = beam_tr.vecEndPos + vecDir;
+						}
+					}
+					else
+					{
+						 //ALERT( at_console, "blocked %f\n", n );
+						flDamage = 0;
+					}
+				//}
+				//else
+				//{
+					//ALERT( at_console, "blocked solid\n" );
+					
+					//flDamage = 0;
+				//}
+
+			}
+		}
+		else
+		{
+			vecSrc = tr.vecEndPos + vecDir;
+			pentIgnore = ENT( pEntity->pev );
+		}
+		nMaxHits--;
+	}
+	
+	lagcomp_end();
+
+
 
 	if (!m_iClip && m_pPlayer->m_rgAmmo[m_iPrimaryAmmoType] <= 0)
 		// HEV suit - indicate out of ammo condition
