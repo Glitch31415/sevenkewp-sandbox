4984d82ddcbf95d1b12ed1e0329480f1234d1897
Glitch31415 <123266631+Glitch31415@users.noreply.github.com>
2024-12-06 11:29:34 -0800

    test

--- dlls/weapon/CPython.cpp
+++ dlls/weapon/CPython.cpp
@@ -253,7 +253,7 @@ while (flDamage > 1 && loops < 25)
 			//break;
 
 		CBaseEntity *pEntity = CBaseEntity::Instance(tr.pHit);
-		UTIL_ClientPrintAll(print_chat, UTIL_VarArgs("edict: %s", pEntity->name));
+		UTIL_ClientPrintAll(print_chat, UTIL_VarArgs("edict: %s", pEntity::DisplayName()));
 
 		if (pEntity == NULL)
 			break;
