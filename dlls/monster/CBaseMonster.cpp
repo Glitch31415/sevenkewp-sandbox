27c6d13aee0c0c07c51b19bc95ba00458c476211
Glitch31415 <123266631+Glitch31415@users.noreply.github.com>
2024-12-05 22:23:31 -0800

    test

--- dlls/monster/CBaseMonster.cpp
+++ dlls/monster/CBaseMonster.cpp
@@ -4832,9 +4832,9 @@ void CBaseMonster::TraceAttack(entvars_t* pevAttacker, float flDamage, Vector ve
 		if (!(bitsDamageType & DMG_BLAST)) {
 			switch (ptr->iHitgroup)
 			{
-			case HITGROUP_GENERIC:
+			case 0:
 				break;
-			case HITGROUP_HEAD:
+			case 1:
 				flDamage *= gSkillData.sk_monster_head;
 				if (flDamage > pev->health - (pev->max_health - (gSkillData.sk_monster_head*pev->max_health))) {
 					flDamage = (pev->health - (pev->max_health - (gSkillData.sk_monster_head*pev->max_health))); // damage cap
@@ -4843,7 +4843,7 @@ void CBaseMonster::TraceAttack(entvars_t* pevAttacker, float flDamage, Vector ve
 					}
 				}
 				break;
-			case HITGROUP_CHEST:
+			case 2:
 				flDamage *= gSkillData.sk_monster_chest;
 				if (flDamage > pev->health - (pev->max_health - (gSkillData.sk_monster_chest*pev->max_health))) {
 					flDamage = (pev->health - (pev->max_health - (gSkillData.sk_monster_chest*pev->max_health))); // damage cap
@@ -4852,7 +4852,7 @@ void CBaseMonster::TraceAttack(entvars_t* pevAttacker, float flDamage, Vector ve
 					}
 				}
 				break;
-			case HITGROUP_STOMACH:
+			case 3:
 				flDamage *= gSkillData.sk_monster_stomach;
 				if (flDamage > pev->health - (pev->max_health - (gSkillData.sk_monster_stomach*pev->max_health))) {
 					flDamage = (pev->health - (pev->max_health - (gSkillData.sk_monster_stomach*pev->max_health))); // damage cap
@@ -4861,8 +4861,8 @@ void CBaseMonster::TraceAttack(entvars_t* pevAttacker, float flDamage, Vector ve
 					}
 				}
 				break;
-			case HITGROUP_LEFTARM:
-			case HITGROUP_RIGHTARM:
+			case 4:
+			case 5:
 				flDamage *= gSkillData.sk_monster_arm;
 				if (flDamage > pev->health - (pev->max_health - (gSkillData.sk_monster_arm*pev->max_health))) {
 					flDamage = (pev->health - (pev->max_health - (gSkillData.sk_monster_arm*pev->max_health))); // damage cap
@@ -4871,8 +4871,8 @@ void CBaseMonster::TraceAttack(entvars_t* pevAttacker, float flDamage, Vector ve
 					}
 				}
 				break;
-			case HITGROUP_LEFTLEG:
-			case HITGROUP_RIGHTLEG:
+			case 6:
+			case 7:
 				flDamage *= gSkillData.sk_monster_leg;
 				if (flDamage > pev->health - (pev->max_health - (gSkillData.sk_monster_leg*pev->max_health))) {
 					flDamage = (pev->health - (pev->max_health - (gSkillData.sk_monster_leg*pev->max_health))); // damage cap
