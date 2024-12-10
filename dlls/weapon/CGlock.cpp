a72657b4fae499da57b8daf19b7baa2587208231
Glitch31415 <123266631+Glitch31415@users.noreply.github.com>
2024-12-10 15:49:05 -0800

    test

--- dlls/weapon/CGlock.cpp
+++ dlls/weapon/CGlock.cpp
@@ -225,11 +225,11 @@ while (flDamage > 1 && loops < 25)
 		{
 			m_pPlayer->pev->effects |= EF_MUZZLEFLASH;
 			fFirstBeam = 0;
-			UTIL_BeamPoints(vecSrc, tr.vecEndPos, MODEL_INDEX("sprites/laserbeam.spr"), 0, 0, 1, 16, 0, RGBA(255, 255, 255, flDamage), 0, NULL, NULL, NULL);
+			UTIL_BeamPoints(vecSrc, tr.vecEndPos, MODEL_INDEX("sprites/laserbeam.spr"), 0, 0, 1, 4, 0, RGBA(255, 255, 255, flDamage), 0, NULL, NULL, NULL);
 
 		}
 		else {
-			UTIL_BeamPoints(vecSrc, tr.vecEndPos, MODEL_INDEX("sprites/laserbeam.spr"), 0, 0, 1, 16, 0, RGBA(255, 255, 255, flDamage), 0, NULL, NULL, NULL);
+			UTIL_BeamPoints(vecSrc, tr.vecEndPos, MODEL_INDEX("sprites/laserbeam.spr"), 0, 0, 1, 4, 0, RGBA(255, 255, 255, flDamage), 0, NULL, NULL, NULL);
 		}
 		float n = 0;
 		if (pEntity->pev->takedamage)
