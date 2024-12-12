2d1b184ddec2f8a52ed7e43673f2d41bb1c42936
Glitch31415 <123266631+Glitch31415@users.noreply.github.com>
2024-12-11 21:00:02 -0800

    rwar

--- scripts/build_game.sh
+++ scripts/build_game.sh
@@ -8,10 +8,5 @@ cd ..
 rm -rf build
 mkdir build
 cd build
-<<<<<<< HEAD
-cmake -DBUILD_SERVER=ON -DINCREASED_SERVER_LIMITS=ON -j 20 ..
-cmake --build . --config Release -j 20
-=======
-cmake -DBUILD_SERVER=ON -DINCREASED_SERVER_LIMITS=OFF ..
+cmake -DBUILD_SERVER=ON -DINCREASED_SERVER_LIMITS=ON ..
 cmake --build . --config Release
->>>>>>> 00d71cc24759aa0e4b7e506965e441c691e27ba6
