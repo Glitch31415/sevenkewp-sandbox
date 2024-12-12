818365622694f9cad8904ad1a483306e21e30b31
Glitch31415 <123266631+Glitch31415@users.noreply.github.com>
2024-12-11 21:00:42 -0800

    test

--- scripts/build_game.sh
+++ scripts/build_game.sh
@@ -8,5 +8,5 @@ cd ..
 rm -rf build
 mkdir build
 cd build
-cmake -DBUILD_SERVER=ON -DINCREASED_SERVER_LIMITS=OF ..
+cmake -DBUILD_SERVER=ON -DINCREASED_SERVER_LIMITS=ON ..
 cmake --build . --config Release
