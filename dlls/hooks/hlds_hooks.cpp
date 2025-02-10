6d2b5c93490901664faf77491b70c195926d1372
Glitch31415 <123266631+Glitch31415@users.noreply.github.com>
2025-02-10 13:00:34 -0800

    test

--- dlls/hooks/hlds_hooks.cpp
+++ dlls/hooks/hlds_hooks.cpp
@@ -709,7 +709,7 @@ void ServerActivate( edict_t *pEdictList, int edictCount, int clientMax )
 		UTIL_PrecacheOther("weapon_inventory");
 	UTIL_PrecacheOther("item_longjump");
 	
-if (!(STRING(gpGlobals->mapname).find(substr) != std::string::npos)) { // i love copy pasting code from the internet
+if (!(STRING(gpGlobals->mapname).find("hl_c") != std::string::npos)) { // i love copy pasting code from the internet
 	UTIL_PrecacheOther("monster_snark");
 UTIL_PrecacheOther("monster_shockroach");
 UTIL_PrecacheOther("monster_rat");
