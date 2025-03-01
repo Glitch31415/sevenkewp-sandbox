d0689ba26c57c61aa027819cf7993369c0b482fc
Glitch31415 <123266631+Glitch31415@users.noreply.github.com>
2025-02-28 16:27:14 -0800

    test

--- dlls/weapon/CShotgun.cpp
+++ dlls/weapon/CShotgun.cpp
@@ -171,6 +171,10 @@ void CShotgun::PrimaryAttack()
 		//vecDir = m_pPlayer->FireBulletsPlayer( 9, vecSrc, vecAiming, VECTOR_CONE_1DEGREES*1.5, 131072, BULLET_PLAYER_BUCKSHOT, 0, 0, m_pPlayer->pev, m_pPlayer->random_seed );
 		for (int i = 0; i < 9; i++) {
 			float flDamage;
+			edict_t		*pentIgnore;
+	TraceResult tr, beam_tr;
+	float flMaxFrac = 1.0;
+	int fFirstBeam = 1;
 
 	UTIL_MakeVectors( m_pPlayer->pev->v_angle + Vector(0, 0, 0) );
 	Vector vecAiming;
@@ -498,6 +502,10 @@ void CShotgun::SecondaryAttack( void )
 		//vecDir = m_pPlayer->FireBulletsPlayer( 18, vecSrc, vecAiming, VECTOR_CONE_1DEGREES*3, 131072, BULLET_PLAYER_BUCKSHOT, 0, 0, m_pPlayer->pev, m_pPlayer->random_seed );
 		for (int i = 0; i < 18; i++) {
 			float flDamage;
+			edict_t		*pentIgnore;
+	TraceResult tr, beam_tr;
+	float flMaxFrac = 1.0;
+	int fFirstBeam = 1;
 
 	UTIL_MakeVectors( m_pPlayer->pev->v_angle + Vector(0, 0, 0) );
 	Vector vecAiming;
